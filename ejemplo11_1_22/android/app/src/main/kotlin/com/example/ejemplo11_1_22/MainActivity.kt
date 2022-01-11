40b849cc319987e9b594d5208c5d00e41f3d9987
Delacruz-6 <gdlcruzguzman@gmail.com>
2022-01-11 08:55:23 +0100

    Añadido ejemplo 11-1-22

---
+++ ejemplo11_1_22/android/app/src/main/kotlin/com/example/ejemplo11_1_22/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ejemplo11_1_22
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_statefull_widget/lib/pages/login.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key, required this.title, required this.photo})
+      : super(key: key);
+
+  final String title;
+  final String photo;
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+          child: Text(
+        widget.photo,
+      )),
+    );
+  }
+}
