f787cb38d8bd0d81e400b9ebeac1a595ba3985ed
Delacruz-6 <gdlcruzguzman@gmail.com>
2022-02-01 08:56:30 +0100

    modificado ejercicio themovieDB

--- flutter_moviedb_homepage/lib/pages/home_page.dart
+++ flutter_moviedb_homepage/lib/pages/home_page.dart
@@ -15,11 +15,61 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body: Column(
         children: [
-          const Center(
-              child: Text(
-            'que tal',
-          )),
-          MoviesPage(),
+          Padding(
+            padding: const EdgeInsets.only(top: 30, left: 20, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    const CircleAvatar(
+                      backgroundImage: AssetImage('assets/img/foto1.jpg'),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Text('Guillermo De la cruz'),
+                    )
+                  ],
+                ),
+                MaterialButton(
+                  onPressed: () {},
+                  color: Colors.grey,
+                  textColor: Colors.white,
+                  child:
+                      Icon(Icons.cast, size: 20, color: Colors.grey.shade300),
+                  padding: EdgeInsets.all(16),
+                  shape: CircleBorder(),
+                )
+              ],
+            ),
+          ),
+          // ignore: prefer_const_constructors
+          Padding(
+            padding: const EdgeInsets.only(right: 150, bottom: 15, top: 15),
+            child: const Text(
+              'Movie, Series,\nTV Shows...',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 270, bottom: 15, top: 15),
+            child: const Text(
+              'Newest',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          const MoviesPage(),
+          Padding(
+            padding: const EdgeInsets.only(right: 270, bottom: 15, top: 15),
+            child: const Text(
+              'Popular',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          const MoviesPage(),
         ],
       ),
     );
